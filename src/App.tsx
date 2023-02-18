696c626ab9335653c40f8eb3819851cac73bc577
unknown <abdi12340.am@gmail.com>
2023-02-18 15:48:26 +0300

    Adding removing Objects in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ getJob=()=>{return this.job}
   const clint =new employee('clint','warehouse operative')
   const po =new employee ('po','security guard')
 
-  class iqrange{
+  class range{
     iq:number;
     people:{name:string,job:string}[]
     constructor(iq:number,people:{name:string,job:string}[]){
@@ -23,11 +23,27 @@ this.iq=iq
 this.people=people
     }
     getIq=()=>{return this.iq}
+
     getpeople=()=>{return this.people}
+
+    addpeople=(people:{name:string,job:string})=>{return this.people.push(people)}
+
+    removepeople=(people:{name:string,job:string})=>{
+      let g:{name:string,job:string}[]=[]
+    
+     g= this.people.filter((i)=>  i!=people)
+      console.log(g)
+      return this.people=g
+    }
   }
   
-  const countryy=new iqrange(80,[clint,po])
+  const countryy=new range(80,[clint,po])
+  const ez=new employee('eezy','sales assistant')
+  countryy.addpeople(ez)
   console.log(countryy)
+  countryy.removepeople(clint);
+  console.log(countryy)
+  
     return(
 <></>
     )
