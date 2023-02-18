f663bd7d8cedc173455aceba358b2a06419b596a
unknown <abdi12340.am@gmail.com>
2023-02-18 16:10:19 +0300

    Abstraction in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ function App() {
   class employee {
     name: string;
     job: string;  
-constructor(name:string,job:string){
+    age:number;
+constructor(name:string,job:string,age:number){
 this.name=name
 this.job=job
+this.age=age
 }
 getName=()=>{return this.name}
 getJob=()=>{return this.job}
+addyear=()=>{this.age=this.age+1}
+setAge=(age)=>{
+  this.age=age
+}
   }
 
-  const clint =new employee('clint','warehouse operative')
-  const po =new employee ('po','security guard')
+  const clint =new employee('clint','warehouse operative',0)
+  const po =new employee ('po','security guard',39)
 
   class range{
     iq:number;
@@ -38,12 +44,17 @@ this.people=people
   }
   
   const countryy=new range(80,[clint,po])
-  const ez=new employee('eezy','sales assistant')
+  const ez=new employee('eezy','sales assistant',20)
   countryy.addpeople(ez)
   console.log(countryy)
-  countryy.removepeople(clint);
-  console.log(countryy)
-  
+
+  const abdirahman=new employee('abdirahman','web developer',21)
+  const country2=new range(125,[abdirahman])
+  const becky=new employee('becky','unemployed',17)
+  country2.addpeople(becky)
+  becky.addyear()
+  abdirahman.addyear()
+  console.log(country2)
     return(
 <></>
     )
