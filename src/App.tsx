2dab81ac36958ce0d35b94bca1fc171fd2da83e2
unknown <abdi12340.am@gmail.com>
2023-02-20 15:27:16 +0300

    Array types in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,37 @@
-import logo from './logo.svg';
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 function App() {
-  class employee {
-    name: string;
-    job: string;  
-    age:number;
-constructor(name:string,job:string,age:number){
-this.name=name
-this.job=job
-this.age=age
+class species{
+ po:string;
+  clint:string
+  constructor(clint:string,po:string){
+    this.clint=clint;
+    this.po=po;
+  }
+  getname=(clint:string,po:string):string=>{
+    return this.clint=clint,this.po=po
+  }
 }
-getName=()=>{return this.name}
-getJob=()=>{return this.job}
-addyear=()=>{this.age=this.age+1}
-getage=()=>{return this.age}
+const speciesy=new species('clint2','po2')
+
+class p extends species{
+  height:(number|string|object)[]
+  weight:number
+  constructor(height:(number|string|object)[],weight:number,clint:string,po:string){
+    super(clint,po)
+    this.height=height;
+this.weight=weight;
   }
+ // getheight=(height:number,weight:number)=>{return this.height=height,this.weight=weight}
+}
 
-  const clint =new employee('clint','warehouse operative',0)
-  const po =new employee ('po','security guard',39)
+const g=new p([9,'poepoeski',90,{name:'clint',age:20}],100,'notclint','po')
 
-  class range{
-    iq:number;
-    people:{name:string,job:string}[]
-    constructor(iq:number,people:{name:string,job:string}[]){
-this.iq=iq
-this.people=people
-    }
-    getIq=()=>{return this.iq}
-
-    getpeople=()=>{return this.people}
-
-    addpeople=(people:{name:string,job:string})=>{return this.people.push(people)}
-
-    removepeople=(people:{name:string,job:string})=>{
-      let g:{name:string,job:string}[]=[]
-    
-     g= this.people.filter((i)=>  i!=people)
-      console.log(g)
-      return this.people=g
-    }
-  }
-  
-
-  const countryy=new range(80,[clint,po])
-  const ez=new employee('eezy','sales assistant',20)
-  countryy.addpeople(ez)
-  console.log(countryy)
-
-  const abdirahman=new employee('abdirahman','web developer',21)
-  const country2=new range(125,[abdirahman])
-  const becky=new employee('becky','unemployed',17)
-  country2.addpeople(becky)
-  becky.addyear()
-  abdirahman.addyear()
-  console.log(country2)
-
-  class dev extends employee{
-    experience:number;
-    language:string;
-constructor(experience:number,language:string,name:string,job:string,age:number){
-  super(name,job,age)
-this.experience=experience
-this.language=language
-}    
-showjob=()=>{ console.log(`Hi! my name is ${this.getName()} im a ${this.getJob()},I am ${this.getage()} years old.
-i program in ${this.language} and i have ${this.experience} years experience`)}
-  }
-
-  const podev=new dev(10,'python','Po poeski','Web developer',45)
-  podev.showjob()
-  console.log(podev)
+console.log(g.height)
+//g.getheight(183,120)
+console.log(g.weight,g.height)
     return(
-    
-<></>
+<>
+</>
     )
   
 }
